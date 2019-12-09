aae6abf8356097d8b9e803db4b5cebce9116abff
jwmcbride1 <john.w.mcbride@gmail.com>
2019-12-08 20:56:21 -0600

    fixed typo in deploy sh Dokerfile

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t jwmcbride1/multi-client:latest -t jwmcbride1/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t jwmcbride1/multi-server:latest -t jwmcbride1/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jwmcbride1/multi-worker:latest -t jwmcbride1/multi-worker:$SHA -f ./worker/Dokcerfile ./worker
+docker build -t jwmcbride1/multi-worker:latest -t jwmcbride1/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push jwmcbride1/multi-client:latest
 docker push jwmcbride1/multi-server:latest
 docker push jwmcbride1/multi-worker:latest
